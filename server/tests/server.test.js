300bdd57cc74f04daecc21bcc92affb5a66424d2
galbo2605 <gal2605@gmail.com>
2018-01-24 23:23:59 +0200

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -290,3 +290,22 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should remove autht token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(e);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
